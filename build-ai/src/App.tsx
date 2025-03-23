a323a57eff6f147286e2da459514c89da98c8e84
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2025-03-23 22:22:55 +0200

    feat: sidebar working with drag and drop + animations for zooming & panning

--- build-ai/src/App.tsx
+++ build-ai/src/App.tsx
@@ -1,48 +1,116 @@
-import { useCallback } from 'react';
+import { useRef, useCallback } from 'react';
 import {
   ReactFlow,
-  Background,
-  Controls,
-  MiniMap,
+  ReactFlowProvider,
   addEdge,
   useNodesState,
   useEdgesState,
-  type OnConnect,
+  Controls,
+  useReactFlow,
+  Background,
   Panel,
 } from '@xyflow/react';
 
 import '@xyflow/react/dist/style.css';
 
-import { initialNodes, nodeTypes } from './nodes';
-import { initialEdges, edgeTypes } from './edges';
-import { AppSidebar } from './sidebar/Sidebar';
+import { Sidebar } from './sidebar/Sidebar';
+import { DnDProvider, useDnD } from './context/DNDProvider';
 
-export default function App() {
-  const [nodes, , onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-  const onConnect: OnConnect = useCallback(
-    (connection) => setEdges((edges) => addEdge(connection, edges)),
-    [setEdges]
+const initialNodes = [
+  {
+    id: '1',
+    type: 'input',
+    data: { label: 'input node' },
+    position: { x: 250, y: 5 },
+  },
+];
+
+let id = 0;
+const getId = () => `dndnode_${id++}`;
+
+const DnDFlow = () => {
+  const reactFlowWrapper = useRef(null);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const { screenToFlowPosition, setViewport, zoomIn, zoomOut } = useReactFlow();
+  const [type] = useDnD();
+
+  const handleTransform = useCallback(() => {
+    setViewport({ x: 0, y: 0, zoom: 1 }, { duration: 800 });
+  }, [setViewport]);
+
+  const onConnect = useCallback(
+    // @ts-ignore
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    []
+  );
+
+  // @ts-ignore
+  const onDragOver = useCallback((event) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+  }, []);
+
+  // @ts-ignore
+  const onDrop = useCallback(
+    // @ts-ignore
+    (event) => {
+      event.preventDefault();
+
+      // check if the dropped element is valid
+      if (!type) {
+        return;
+      }
+
+      const position = screenToFlowPosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+      const newNode = {
+        id: getId(),
+        type,
+        position,
+        data: { label: `${type} node` },
+      };
+
+      // @ts-ignore
+      setNodes((nds) => nds.concat(newNode));
+    },
+    [screenToFlowPosition, type]
   );
 
   return (
-    <ReactFlow
-      nodes={nodes}
-      nodeTypes={nodeTypes}
-      onNodesChange={onNodesChange}
-      edges={edges}
-      edgeTypes={edgeTypes}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
-      fitView
-    >
-      <Background />
-      <MiniMap />
-      <Controls />
-
-      <Panel position="top-left">
-        <AppSidebar />
+    <div className="dndflow">
+      <Sidebar />
+      <div className="reactflow-wrapper" ref={reactFlowWrapper}>
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          onDrop={onDrop}
+          onDragOver={onDragOver}
+          fitView
+          style={{ backgroundColor: '#F7F9FB' }}
+        >
+          <Controls />
+          <Background />
+        </ReactFlow>
+      </div>
+      <Panel position="top-right">
+        <button onClick={() => zoomIn({ duration: 800 })}>zoom in</button>
+        <button onClick={() => zoomOut({ duration: 800 })}>zoom out</button>
+        <button onClick={handleTransform}>pan to center(0,0,1)</button>
       </Panel>
-    </ReactFlow>
+    </div>
   );
-}
+};
+
+export default () => (
+  <ReactFlowProvider>
+    <DnDProvider>
+      <DnDFlow />
+    </DnDProvider>
+  </ReactFlowProvider>
+);
--- build-ai/src/sidebar/Sidebar.tsx
+++ build-ai/src/sidebar/Sidebar.tsx
@@ -1,20 +1,44 @@
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarHeader,
-} from '../components/ui/sidebar';
+import React from 'react';
+import { useDnD } from '../context/DNDProvider';
 
-export function AppSidebar() {
+export const Sidebar = () => {
+  const [_, setType] = useDnD();
+  console.log(_);
+
+  const onDragStart = (
+    event: React.DragEvent<HTMLDivElement>,
+    nodeType: string
+  ) => {
+    if (nodeType) setType!(nodeType);
+    event.dataTransfer.effectAllowed = 'move';
+  };
+
   return (
-    <Sidebar>
-      <SidebarHeader />
-      <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
-      </SidebarContent>
-      <SidebarFooter />
-    </Sidebar>
+    <aside>
+      <div className="description">
+        <h1>Create your Agent</h1>
+      </div>
+      <div
+        className="dndnode input"
+        onDragStart={(event) => onDragStart(event, 'input')}
+        draggable
+      >
+        Input Node
+      </div>
+      <div
+        className="dndnode"
+        onDragStart={(event) => onDragStart(event, 'default')}
+        draggable
+      >
+        Default Node
+      </div>
+      <div
+        className="dndnode output"
+        onDragStart={(event) => onDragStart(event, 'output')}
+        draggable
+      >
+        Output Node
+      </div>
+    </aside>
   );
-}
+};
