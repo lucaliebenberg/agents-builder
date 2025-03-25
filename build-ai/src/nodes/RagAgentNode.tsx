8fe46a6e84e0219cd5f8e4c11a98cde04b336bc4
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2025-03-25 22:08:42 +0200

    chore: commit to update vc

--- build-ai/src/nodes/RagAgentNode.tsx
+++ build-ai/src/nodes/RagAgentNode.tsx
@@ -87,15 +87,17 @@ export function RagAgentNode({ id, data, selected }: NodeProps<RagAgentNode>) {
           />
         </div>
 
-        <div className="flex flex-col flex-wrap w-[300px]">
-          <label className="block text-sm font-medium text-gray-700">
-            System Prompt
-          </label>
+        <div className="flex flex-col flex-wrap w-fill">
+          <div className="w-fill ">
+            <label className="items-start w-auto text-left text-sm font-medium text-gray-700">
+              System Prompt
+            </label>
+          </div>
           <textarea
             value={agentConfig.systemPrompt}
             onChange={(e) => handleInputChange('systemPrompt', e.target.value)}
             className="mt-1 block w-auto  pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-            rows={3}
+            cols={16}
             placeholder="Enter system prompt..."
           />
         </div>
